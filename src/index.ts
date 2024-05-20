93a4829a11b9cb86db858224376fe188addb7169
Lexa <sashavain@icloud.com>
2024-05-20 21:14:19 +0300

    123

--- src/index.ts
+++ src/index.ts
@@ -10,83 +10,73 @@ const COOKIE: string = config.COOCKIEFORYOUTUBE;
 const TOKEN: string = config.DISCORDBOTTOKEN;
 
 if (!(COOKIE && TOKEN)) {
-  throw new TypeError("Config.json. Please insert COOKIE Netscape or Tokens");
+	throw new TypeError("Config.json. Please insert COOKIE Netscape or Tokens");
 }
 
 import { checkStatusbot } from "./voice-status/check-status-voice";
 import { handleCommands } from "./handler-commands/handler-commands";
 
 import { botReplys } from "./consts/bot-replys";
-import { commandList } from "./consts/command-list";
+
 
 export type mapPlayers = Map<string, playerDiscordBot>;
 const mapPlayers = new Map<string, playerDiscordBot>();
 
 play.setToken({
-  youtube: {
-    cookie: COOKIE,
-  },
+	youtube: {
+		cookie: COOKIE,
+	},
 });
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.GuildBans,
-    GatewayIntentBits.AutoModerationConfiguration,
-    GatewayIntentBits.AutoModerationExecution,
-    GatewayIntentBits.DirectMessageReactions,
-    GatewayIntentBits.DirectMessageTyping,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.GuildBans,
-    GatewayIntentBits.GuildEmojisAndStickers,
-    GatewayIntentBits.GuildIntegrations,
-    GatewayIntentBits.GuildInvites,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessageReactions,
-    GatewayIntentBits.GuildMessageTyping,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildModeration,
-    GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.GuildScheduledEvents,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildWebhooks,
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.MessageContent,
-    // GatewayIntentBits.GuildAuditLogs,
-  ],
-  // partials: ["CHANNEL", "MESSAGE"],
+	intents: [
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.AutoModerationConfiguration,
+		GatewayIntentBits.AutoModerationExecution,
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.DirectMessageTyping,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.GuildIntegrations,
+		GatewayIntentBits.GuildInvites,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildModeration,
+		GatewayIntentBits.GuildPresences,
+		GatewayIntentBits.GuildScheduledEvents,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildWebhooks,
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.MessageContent,
+		// GatewayIntentBits.GuildAuditLogs,
+	],
+	// partials: ["CHANNEL", "MESSAGE"],
 });
 
 // @ts-ignore
 client.on("messageCreate", async (message: Message) => {
-  if (message.author.bot || !message.guild?.id) return;
-  //i think it's happened never but type ts say it's optional
+	if (message.author.bot || !message.guild?.id) return;
 
-  const isPlay = message.content.startsWith("?play");
+	const voiceChannel = message.member?.voice?.channel;
+	if (!voiceChannel)
+		return await message.channel.send(botReplys.userNotInVoice);
 
-  if (commandList.includes(message.content) || !isPlay) {
-    const voiceChannel = message.member?.voice?.channel;
-    if (!voiceChannel)
-      return await message.channel.send(botReplys.userNotInVoice);
-  }
-
-  const player = mapPlayers.get(message.guild.id);
-
-  if (!player && !isPlay) {
-    return await message.channel.send(botReplys.playerNotPlaying);
-  }
-
-  await handleCommands(player, message, mapPlayers, client);
+	const player = mapPlayers.get(message.guild.id);
+	await handleCommands(player, message, mapPlayers, client);
 });
 
 client.on(
-  "voiceStateUpdate",
-  async (oldState: VoiceState, newState: VoiceState) => {
-    await checkStatusbot(oldState, newState, mapPlayers);
-  }
+	"voiceStateUpdate",
+	async (oldState: VoiceState, newState: VoiceState) => {
+		await checkStatusbot(oldState, newState, mapPlayers);
+	},
 );
 
 client.on("ready", () => {
-  console.log(`We have logged in as ${client?.user?.tag}!`);
+	console.log(`We have logged in as ${client?.user?.tag}!`);
 });
 
 client.login(TOKEN);
