43413d5216ffc9783721f8341aa2f7507c8aee0b
Lexa <sashavain@icloud.com>
2024-05-20 17:23:30 +0300

    added forgeted await

--- src/commands/pause.ts
+++ src/commands/pause.ts
@@ -8,5 +8,5 @@ export async function pause(player: playerDiscordBot, message: Message) {
 	if (player.Audioplayer.state.status === AudioPlayerStatus.Paused) {
 	}
 	player.pause();
-	return message.channel.send(botReplys.trackOnPause);
+	return await message.channel.send(botReplys.trackOnPause);
 }
